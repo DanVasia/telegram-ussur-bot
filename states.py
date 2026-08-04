bdf35b333da12cc371ac0fcf105027bd221ee5be
DanVasia <fangag524@gmail.com>
2026-08-04 11:06:22 +1000

    Update states.py

--- states.py
+++ states.py
@@ -1,6 +1,8 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class NewsForm(StatesGroup):
-    текст = State()
-    анонимный = State()
-    медиа = State()   
+    текст = State()      # первый шаг – текст новости
+    возраст = State()    # второй шаг – возраст
+    имя = State()        # третий шаг – имя
+    район = State()      # четвёртый шаг – район
+    анонимный = State()  # пятый шаг – выбор анонимности 
